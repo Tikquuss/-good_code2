ab76d070015c5e84e2e6597ec822f20e0cca5589
Tikquuss <pascalnotsawo@gmail.com>
2023-02-10 02:14:56 -0500

    update

--- multiple_runs.py
+++ multiple_runs.py
@@ -1,10 +1,23 @@
 import itertools
 import submitit
+from multiprocessing import Process
+from functools import partial
 
 from train import train, get_default_params, AttrDict
 
+def run_task_list(function, task_list, id=None) :
+    for command in task_list : function(command)
+
+def run_task_dict(function, task_list_dict) :
+    all_process = []
+    for id, task_list in task_list_dict.items() :
+        task = partial(run_task_list, function, task_list, id)
+        p = Process(target=task)
+        p.start()
+        all_process.append(p)
+    for p in all_process : p.join()
+
 if __name__ == "__main__":
-    
 
     params = get_default_params()
 
@@ -33,6 +46,8 @@ if __name__ == "__main__":
     params.max_epochs=2#0000
     params.every_n_epochs=5000 # save every - epochs
     params.accelerator = "gpu" #"auto"
+
+    #params.devices = [0] # "auto"
     params.devices = 1 # "auto"
 
     params.use_wandb=False#True
@@ -58,8 +73,8 @@ if __name__ == "__main__":
         params["weight_decay"] = weight_decay
         params["group_name"] = f"wd={weight_decay}-lr={max_lr}"
 
-        logdir=f"{dump_path}/logs/{params.train_data_pct}/{params.group_name}"
-        datadir=f"{dump_path}/data/{params.train_data_pct}/{params.group_name}"
+        params.logdir=f"{dump_path}/logs/{params.train_data_pct}/{params.group_name}"
+        params.datadir=f"{dump_path}/data/{params.train_data_pct}/{params.group_name}"
 
         for random_seed in [0, 100] :
             params.random_seed=random_seed
@@ -69,9 +84,27 @@ if __name__ == "__main__":
             for att_name in params.keys() : setattr(command, att_name, getattr(params, att_name))
             commands.append(command)
 
-    # run experiment
+    """
+    The current implementation does not support multi-devices training. 
+    So when we pass a list of gpus (tpu, ...) as devices, we distribute them in 
+    such a way that each run is only done on one gpu
+    """
+    ids_for_task = []
+    if type(params.devices) == list :
+        L = len(params.devices)
+        i = 0
+        for j in range(len(commands)):
+            command[j].devices = [params.devices[i]]
+            ids_for_task.append(i)
+            i%=L
+
+    ## run experiment
+
+    # 
     slurm_partition = None # TODO
     if slurm_partition is not None:
+        # slurm
+        # https://github.com/facebookincubator/submitit/blob/main/docs/examples.md
         slurm_output_dir = f"{dump_path}/logs" # TODO
         max_time = None # TODO
         executor = submitit.SlurmExecutor(folder=slurm_partition)
@@ -83,5 +116,11 @@ if __name__ == "__main__":
             partition=slurm_partition)
         executor.map_array(train, commands)
     else:
-        for command in commands:
-            logdir = train(command)
+        # When we have multiple devices as described above, we launch in parallel a sequence of runs on each device
+        if type(params.devices) == list :
+            task_list_dict = {}
+            for i, command in enumerate(commands) :
+                task_list_dict[ids_for_task[i]] = task_list_dict.get(ids_for_task[i], []) + [command]
+            run_task_dict(train, task_list_dict)
+        else :
+            for command in commands: logdir = train(command)
